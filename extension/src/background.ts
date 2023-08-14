ed8408e9d5286798ab75396e8f2b9eb427b1bcba
tornikeo <tornikeonoprishvili@gmail.com>
2023-08-14 16:03:21 +0400

    Add sample ctx rmb menu

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -5,6 +5,7 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'GREETINGS') {
     const message: string = `Hi ${
@@ -18,4 +19,57 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       message,
     });
   }
+});
+
+
+chrome.runtime.onInstalled.addListener(function () {
+  // Create one test item for each context type.
+  let contexts = [
+    'page',
+    'selection',
+    'link',
+    'editable',
+    'image',
+    'video',
+    'audio'
+  ];
+
+  for (let i = 0; i < contexts.length; i++) {
+    let context = contexts[i];
+    let title = "Test '" + context + "' menu item";
+    chrome.contextMenus.create({
+      title: title,
+      contexts: [<chrome.contextMenus.ContextType>context],
+      id: context
+    });
+  }
+    // Create a parent item and two children.
+    let parent = chrome.contextMenus.create({
+      title: 'Test parent item',
+      id: 'parent'
+    });
+    chrome.contextMenus.create({
+      title: 'Child 1',
+      parentId: parent,
+      id: 'child1'
+    });
+    chrome.contextMenus.create({
+      title: 'Child 2',
+      parentId: parent,
+      id: 'child2'
+    });
+  
+    // Create a radio item.
+    chrome.contextMenus.create({
+      title: 'radio',
+      type: 'radio',
+      id: 'radio'
+    });
+  
+    // Create a checkbox item.
+    chrome.contextMenus.create({
+      title: 'checkbox',
+      type: 'checkbox',
+      id: 'checkbox'
+    });
 });
